5f3bd66e3add52573ff64f3b68c767e6fb58b856
David  Salinas <david.salinas.pro@gmail.com>
2025-02-24 11:28:10 +0100

    move slurm status sacct tools to dedicated file

--- slurmpilot/slurm_job_status.py
+++ slurmpilot/slurm_job_status.py
@@ -1,3 +1,16 @@
+from collections import defaultdict
+from dataclasses import dataclass
+from datetime import datetime
+from pathlib import Path
+
+import pandas as pd
+from pandas._libs.tslibs.parsing import DateParseError
+
+from slurmpilot.job_metadata import JobMetadata
+from slurmpilot.remote_command import RemoteExecution
+from slurmpilot.util import parse_nseconds_slurm_status
+
+
 class SlurmJobStatus:
     completed: str = "COMPLETED"
     pending: str = "PENDING"
@@ -9,3 +22,164 @@ class SlurmJobStatus:
 
     def statuses(self):
         return [self.completed, self.pending, self.failed, self.running, self.cancelled]
+
+
+@dataclass
+class SlurmJobInfo:
+    jobname: str
+    cluster: str
+    JobID: str
+    task_id: int | None  # for job-array
+    Elapsed: str
+    creation: str
+    Start: datetime
+    State: str
+    NodeList: str
+
+
+def job_infos(
+    connections, jobid_from_jobname, jobnames: list[str]
+) -> list[SlurmJobInfo]:
+    if not isinstance(jobnames, list):
+        assert isinstance(jobnames, str)
+        jobnames = [jobnames]
+
+    # first, we build a dictionary mapping clusters to job information
+    jobid_mapping = {}
+    clusters = defaultdict(list)
+    for jobname in jobnames:
+        # TODO support having this one missing (
+        jobid = jobid_from_jobname(jobname)
+        jobid_mapping[jobid] = jobname
+        job_metadata = JobMetadata.from_jobname(jobname)
+        if job_metadata is not None:
+            cluster = job_metadata.cluster
+            if jobid is not None:
+                clusters[cluster].append((jobid, job_metadata))
+
+    # second, we call sacct on each clusters with the corresponding jobs
+    rows = []
+    for cluster in clusters.keys():
+        job_clusters = clusters[cluster]
+        # filter jobs with missing jobid
+        job_and_ids = [
+            (job_metadata, jobid)
+            for (jobid, job_metadata) in job_clusters
+            if jobid is not None
+        ]
+        rows += call_and_parse_sacct(
+            cluster=cluster,
+            job_and_ids=job_and_ids,
+            connections=connections,
+        )
+    return list(sorted(rows, key=lambda x: x.creation, reverse=True))
+
+
+def call_and_parse_sacct(
+    connections: dict[str, RemoteExecution],
+    cluster: str,
+    job_and_ids: list[JobMetadata, int],
+) -> list[SlurmJobInfo]:
+    # calls sacct on the requested jobids for a given cluster
+    jobinfos, jobids = zip(*job_and_ids)
+    jobid_mapping = {jobid: job for job, jobid in job_and_ids}
+    sacct_format = "JobID,Elapsed,start,State,nodelist"
+    job_ids = ",".join([str(x) for x in jobids])
+    try:
+        cmd_res = connections[cluster].run(
+            f'sacct --format="{sacct_format}" -X -p --jobs={job_ids}'
+        )
+    except (KeyError, ValueError) as e:
+        return []
+    sacct_string = cmd_res.stdout
+
+    # parse sacct_string
+    lines = sacct_string.split("\n")
+    keys = lines[0].split("|")[:-1]
+    rows = []
+    for line in lines[1:]:
+        if len(line) > 0:
+            kwargs = dict(zip(keys, line.split("|")))
+            if "JobID" in kwargs:
+                slurm_jobid = kwargs.get("JobID")
+                if "_" in slurm_jobid:
+                    parent_jobid, task_id = kwargs.get("JobID").split("_")
+                    try:
+                        task_id = int(task_id)
+                    except ValueError:
+                        task_id = None
+                else:
+                    parent_jobid = kwargs.get("JobID")
+                    task_id = None
+                jobinfo = jobid_mapping.get(int(parent_jobid))
+                try:
+                    kwargs["Start"] = pd.to_datetime(kwargs["Start"]).strftime(
+                        "%d/%m/%y-%H:%M"
+                    )
+                except DateParseError:
+                    kwargs["Start"] = None
+                rows.append(
+                    SlurmJobInfo(
+                        jobname=jobinfo.jobname,
+                        task_id=task_id,
+                        cluster=cluster,
+                        creation=jobinfo.date,
+                        **kwargs,
+                    )
+                )
+    return rows
+
+
+def print_jobs(
+    jobinfos: list[SlurmJobInfo],
+    n_jobs: int = 10,
+    max_colwidth: int = 50,
+    status_verbose: bool = True,
+):
+    rows = []
+    print("Calling remote sacct on remote nodes to get status.")
+
+    for jobinfo in jobinfos:
+        status = jobinfo.State
+        # TODO status when missing jobid.json => error at launching
+        if status is None:
+            status_symbol = "Unknown slurm status 🏝"
+        else:
+            status_mapping = {
+                SlurmJobStatus.out_of_memory: "OOM 🤯",
+                SlurmJobStatus.failed: "Slurm job failed ❌",
+                SlurmJobStatus.pending: "Pending ⏳",
+                SlurmJobStatus.running: "️Running 🏃",
+                SlurmJobStatus.completed: "Completed ✅",
+                SlurmJobStatus.cancelled: "Canceled ⚠️",
+            }
+            if "CANCELLED" in status:
+                status_symbol = "Cancelled ⚠️"
+            else:
+                status_symbol = status_mapping.get(status, f"Unknown state: {status}")
+        n_seconds = parse_nseconds_slurm_status(jobinfo.Elapsed)
+        task_id = "" if jobinfo.task_id is None else f" ({jobinfo.task_id})"
+        # right now, results are provided sorted by creation date and then invert taskid
+        # TODO we should support not listing all tasks of a job-array
+        rows.append(
+            {
+                "job": Path(jobinfo.jobname).name + task_id,
+                "cluster": jobinfo.cluster,
+                "creation": jobinfo.creation,
+                "min": f"{n_seconds / 60.:.1f}",
+                "status": (
+                    f"{status_symbol} " if status_verbose else f"{status_symbol[-1:]} "
+                ),
+                "NodeList": jobinfo.NodeList,
+                # "full jobname": jobinfo.jobname,
+            }
+        )
+    if n_jobs is not None:
+        rows = rows[:n_jobs]
+    df = pd.DataFrame(rows)
+    print(
+        df.to_string(
+            index=False,
+            max_colwidth=max_colwidth,
+        )
+    )
--- slurmpilot/slurm_wrapper.py
+++ slurmpilot/slurm_wrapper.py
@@ -6,13 +6,9 @@ import shutil
 import time
 import traceback
 from collections import defaultdict
-from dataclasses import dataclass
 from datetime import datetime
 from pathlib import Path
 from typing import List
-
-import pandas as pd
-from pandas._libs.tslibs.parsing import DateParseError
 
 from slurmpilot.callback import SlurmSchedulerCallback, format_highlight
 from slurmpilot.config import Config, load_config
@@ -23,9 +19,14 @@ from slurmpilot.remote_command import (
     RemoteExecution,
     RemoteCommandExecutionSubprocess,
 )
-from slurmpilot.slurm_job_status import SlurmJobStatus
+from slurmpilot.slurm_job_status import (
+    SlurmJobStatus,
+    print_jobs,
+    call_and_parse_sacct,
+    SlurmJobInfo,
+    job_infos,
+)
 from slurmpilot.slurm_main_script import generate_main_slurm_script
-from slurmpilot.util import catchtime, parse_nseconds_slurm_status
 
 logger = logging.getLogger(__name__)
 logging.basicConfig(
@@ -417,158 +418,28 @@ class SlurmWrapper:
 
     def status(self, jobnames: list[str]) -> list[str | None]:
         # TODO handle case of status missing
-        jobinfos = self.job_infos(jobnames)
+        jobinfos = job_infos(
+            jobid_from_jobname=self.jobid_from_jobname,
+            jobnames=jobnames,
+            connections=self.connections,
+        )
         return [jobinfo.State for jobinfo in jobinfos]
-
-    @dataclass
-    class SlurmJobInfo:
-        jobname: str
-        cluster: str
-        JobID: str
-        task_id: int | None  # for job-array
-        Elapsed: str
-        creation: str
-        Start: datetime
-        State: str
-        NodeList: str
-
-    def job_infos(self, jobnames: list[str]) -> list["SlurmJobInfo"]:
-        if not isinstance(jobnames, list):
-            assert isinstance(jobnames, str)
-            jobnames = [jobnames]
-
-        # first, we build a dictionary mapping clusters to job information
-        jobid_mapping = {}
-        clusters = defaultdict(list)
-        for jobname in jobnames:
-            # TODO support having this one missing (
-            jobid = self.jobid_from_jobname(jobname)
-            jobid_mapping[jobid] = jobname
-            job_metadata = JobMetadata.from_jobname(jobname)
-            if job_metadata is not None:
-                cluster = job_metadata.cluster
-                if jobid is not None:
-                    clusters[cluster].append((jobid, job_metadata))
-
-        # second, we call sacct on each clusters with the corresponding jobs
-        rows = []
-        for cluster in clusters.keys():
-            job_clusters = clusters[cluster]
-            # filter jobs with missing jobid
-            job_and_ids = [
-                (job_metadata, jobid)
-                for (jobid, job_metadata) in job_clusters
-                if jobid is not None
-            ]
-            rows += self._jobinfo_cluster(cluster=cluster, job_and_ids=job_and_ids)
-        return list(sorted(rows, key=lambda x: x.creation, reverse=True))
-
-    def _jobinfo_cluster(
-        self, cluster: str, job_and_ids: list[JobMetadata, int]
-    ) -> list[SlurmJobInfo]:
-        # calls sacct on the requested jobids for a given cluster
-        jobinfos, jobids = zip(*job_and_ids)
-        jobid_mapping = {jobid: job for job, jobid in job_and_ids}
-        sacct_format = "JobID,Elapsed,start,State,nodelist"
-        job_ids = ",".join([str(x) for x in jobids])
-        try:
-            cmd_res = self.connections[cluster].run(
-                f'sacct --format="{sacct_format}" -X -p --jobs={job_ids}'
-            )
-        except (KeyError, ValueError) as e:
-            return []
-        sacct_string = cmd_res.stdout
-
-        # parse sacct_string
-        lines = sacct_string.split("\n")
-        keys = lines[0].split("|")[:-1]
-        rows = []
-        for line in lines[1:]:
-            if len(line) > 0:
-                kwargs = dict(zip(keys, line.split("|")))
-                if "JobID" in kwargs:
-                    slurm_jobid = kwargs.get("JobID")
-                    if "_" in slurm_jobid:
-                        parent_jobid, task_id = kwargs.get("JobID").split("_")
-                        try:
-                            task_id = int(task_id)
-                        except ValueError:
-                            task_id = None
-                    else:
-                        parent_jobid = kwargs.get("JobID")
-                        task_id = None
-                    jobinfo = jobid_mapping.get(int(parent_jobid))
-                    try:
-                        kwargs["Start"] = pd.to_datetime(kwargs["Start"]).strftime(
-                            "%d/%m/%y-%H:%M"
-                        )
-                    except DateParseError:
-                        kwargs["Start"] = None
-                    rows.append(
-                        self.SlurmJobInfo(
-                            jobname=jobinfo.jobname,
-                            task_id=task_id,
-                            cluster=cluster,
-                            creation=jobinfo.date,
-                            **kwargs,
-                        )
-                    )
-        return rows
 
     def print_jobs(
         self, n_jobs: int = 10, max_colwidth: int = 50, status_verbose: bool = True
     ):
-        rows = []
         jobs = self.list_metadatas(n_jobs)
         print("Calling remote sacct on remote nodes to get status.")
-        jobinfos = self.job_infos(jobnames=[job.jobname for job in jobs])
-        for jobinfo in jobinfos:
-            status = jobinfo.State
-            # TODO status when missing jobid.json => error at launching
-            if status is None:
-                status_symbol = "Unknown slurm status 🏝"
-            else:
-                status_mapping = {
-                    SlurmJobStatus.out_of_memory: "OOM 🤯",
-                    SlurmJobStatus.failed: "Slurm job failed ❌",
-                    SlurmJobStatus.pending: "Pending ⏳",
-                    SlurmJobStatus.running: "️Running 🏃",
-                    SlurmJobStatus.completed: "Completed ✅",
-                    SlurmJobStatus.cancelled: "Canceled ⚠️",
-                }
-                if "CANCELLED" in status:
-                    status_symbol = "Cancelled ⚠️"
-                else:
-                    status_symbol = status_mapping.get(
-                        status, f"Unknown state: {status}"
-                    )
-            n_seconds = parse_nseconds_slurm_status(jobinfo.Elapsed)
-            task_id = "" if jobinfo.task_id is None else f" ({jobinfo.task_id})"
-            # right now, results are provided sorted by creation date and then invert taskid
-            # TODO we should support not listing all tasks of a job-array
-            rows.append(
-                {
-                    "job": Path(jobinfo.jobname).name + task_id,
-                    "cluster": jobinfo.cluster,
-                    "creation": jobinfo.creation,
-                    "min": f"{n_seconds / 60.:.1f}",
-                    "status": (
-                        f"{status_symbol} "
-                        if status_verbose
-                        else f"{status_symbol[-1:]} "
-                    ),
-                    "NodeList": jobinfo.NodeList,
-                    # "full jobname": jobinfo.jobname,
-                }
-            )
-        if n_jobs is not None:
-            rows = rows[:n_jobs]
-        df = pd.DataFrame(rows)
-        print(
-            df.to_string(
-                index=False,
-                max_colwidth=max_colwidth,
-            )
+        jobinfos = job_infos(
+            jobid_from_jobname=self.jobid_from_jobname,
+            jobnames=[job.jobname for job in jobs],
+            connections=self.connections,
+        )
+        print_jobs(
+            jobinfos=jobinfos,
+            n_jobs=n_jobs,
+            max_colwidth=max_colwidth,
+            status_verbose=status_verbose,
         )
 
     def download_job(self, jobname: str | None = None):
