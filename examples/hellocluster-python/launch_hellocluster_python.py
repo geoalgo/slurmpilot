70acc683e533b761d528e43353497d7b6e4a1718
David  Salinas <david.salinas.pro@gmail.com>
2025-03-21 16:23:08 +0100

    revert unwanwted change in example

--- examples/hellocluster-python/launch_hellocluster_python.py
+++ examples/hellocluster-python/launch_hellocluster_python.py
@@ -5,9 +5,7 @@ from slurmpilot.util import unify
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
-    # cluster, partition = default_cluster_and_partition()
-    cluster = "kislurm"
-    partition = "ml_gpu-rtx2080"
+    cluster, partition = default_cluster_and_partition()
     # make the jobname unique by appending a coolname
     jobname = unify("examples/hello-cluster-python/job", method="coolname")
     slurm = SlurmPilot(clusters=[cluster])
